117c93a9499367f395573c9241a3b5b751b6ce35
navjotsinh189 <navjotsingh189@gmail.com>
2017-10-20 17:59:24 +0000

    task3

---
+++ final--task3.py
@@ -0,0 +1,10 @@
+file=open('running-config.cfg')
+file1=open('newconfigfile','w')
+
+
+def replace_ip():
+ for line in file:
+   file1.write(line.replace('172','192'))
+ file1.close() 
+ file.close()
+replace_ip()
